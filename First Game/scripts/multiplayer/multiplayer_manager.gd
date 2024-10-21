227ed2b47420ff145cd8e529fd00700f3260742e
Ray46501 <raiyan.rahman1@education.nsw.gov.au>
2024-10-22 10:39:31 +1100

    created first server 22/10

--- First Game/scripts/multiplayer/multiplayer_manager.gd
+++ First Game/scripts/multiplayer/multiplayer_manager.gd
@@ -1,7 +1,15 @@
 extends Node
 
+const SERVER_PORT = 8080
+const SERVER_IP = "127.0.0.1"
+
 func become_host():
 	print("Starting Host ...")
+	
+	var server_peer = ENetMultiplayerPeer.new()
+	server_peer.create_server(SERVER_PORT)
+	
+	multiplayer.multiplayer_peer = server_peer
 	
 func join():
 	print("New Player Joining")
