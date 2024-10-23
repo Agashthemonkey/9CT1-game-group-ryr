c94660964fe61f4c17438e698cedb8b79224f2fc
Ray46501 <raiyan.rahman1@education.nsw.gov.au>
2024-10-24 10:29:38 +1100

    commit 24/10

--- First Game/scripts/multiplayer/multiplayer_manager.gd
+++ First Game/scripts/multiplayer/multiplayer_manager.gd
@@ -5,8 +5,12 @@ const SERVER_IP = "127.0.0.1"
 
 var multiplayer_scene = preload("res://scenes/multiplayer_player.tscn")
 
+var _players_spawn_node
+
 func become_host():
 	print("Starting Host ...")
+	
+	_players_spawn_node = get_tree().get_current_scene().get_node("Players")
 	
 	var server_peer = ENetMultiplayerPeer.new()
 	server_peer.create_server(SERVER_PORT)
@@ -28,6 +32,10 @@ func _add_player_to_game(id: int):
 	print("Player %s joined the game!" % id)
 	
 	var player_to_add = multiplayer_scene.instantiate()
+	player_to_add.player_id = id
+	player_to_add.name = str(id)
+	
+	_players_spawn_node.add_child(player_to_add, true)
 	
 func _del_player(id: int):
 	print("Player %s left the game!" % id)
